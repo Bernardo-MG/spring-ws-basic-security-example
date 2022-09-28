00862b9d9ab5da1a0bec1a659e0a543688580a23
Bernardo <programming@bernardomg.com>
2022-09-28 23:47:25 +0200

    Cleaned up code

--- src/main/java/com/bernardomg/example/ws/security/basic/domain/user/service/DefaultUserService.java
+++ src/main/java/com/bernardomg/example/ws/security/basic/domain/user/service/DefaultUserService.java
@@ -1,25 +1,41 @@
+/**
+ * The MIT License (MIT)
+ * <p>
+ * Copyright (c) 2022 the original author or authors.
+ * <p>
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * <p>
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * <p>
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 
 package com.bernardomg.example.ws.security.basic.domain.user.service;
 
-import java.util.Objects;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.bernardomg.example.ws.security.basic.domain.user.model.User;
 import com.bernardomg.example.ws.security.basic.domain.user.repository.PersistentUserRepository;
 
+import lombok.AllArgsConstructor;
+
 @Service
+@AllArgsConstructor
 public final class DefaultUserService implements UserService {
 
     private final PersistentUserRepository repository;
-
-    @Autowired
-    public DefaultUserService(final PersistentUserRepository repo) {
-        super();
-
-        repository = Objects.requireNonNull(repo);
-    }
 
     @Override
     public final Iterable<? extends User> getUsers() {
