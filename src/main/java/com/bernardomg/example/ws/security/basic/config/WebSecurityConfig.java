75f6e683c994ae622d06d0ed1a16c7bfebfe8516
Bernardo <programming@bernardomg.com>
2022-10-01 21:49:31 +0200

    Removed old security config and added test

--- src/main/java/com/bernardomg/example/ws/security/basic/config/WebSecurityConfig.java
+++ src/main/java/com/bernardomg/example/ws/security/basic/config/WebSecurityConfig.java
@@ -30,7 +30,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.ExpressionUrlAuthorizationConfigurer;
 import org.springframework.security.config.annotation.web.configurers.FormLoginConfigurer;
 import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
@@ -84,12 +83,6 @@ public class WebSecurityConfig {
         http.userDetailsService(userDetailsService);
 
         return http.build();
-    }
-
-    @Bean
-    public WebSecurityCustomizer webSecurityCustomizer() {
-        return (web) -> web.ignoring()
-            .antMatchers("/login/**");
     }
 
 }
