e0b5d825bb6f6f9ca0c2ea5123b835c2f114afa2
Bernardo <programming@bernardomg.com>
2022-10-06 15:41:06 +0200

    Encapsulated login validation

--- src/main/java/com/bernardomg/example/ws/security/basic/auth/login/service/DefaultLoginService.java
+++ src/main/java/com/bernardomg/example/ws/security/basic/auth/login/service/DefaultLoginService.java
@@ -63,12 +63,44 @@ public final class DefaultLoginService implements LoginService {
 
     @Override
     public final LoginStatus login(final String username, final String password) {
-        final Boolean         logged;
-        final LoginStatus     status;
-        final String          token;
-        Optional<UserDetails> details;
+        final Boolean     logged;
+        final LoginStatus status;
+        final String      token;
 
         log.debug("Log in attempt for {}", username);
+
+        logged = validateUser(username, password);
+
+        // Generate token
+        token = generateToken(username, password);
+
+        status = new ImmutableLoginStatus(username, logged, token);
+
+        log.debug("Finished log in attempt for {}. Logged in: {}", username, logged);
+
+        return status;
+    }
+
+    private final String generateToken(final String username, final String password) {
+        final String rawToken;
+
+        rawToken = String.format("%s:%s", username, password);
+        return Base64.getEncoder()
+            .encodeToString(rawToken.getBytes(StandardCharsets.UTF_8));
+    }
+
+    /**
+     * Checks if the user is valid and allows to attempt a login.
+     * 
+     * @param username
+     *            username to authenticate
+     * @param password
+     *            password to authenticate
+     * @return {@true} if the user is valid, {@code false} otherwise
+     */
+    private final Boolean validateUser(final String username, final String password) {
+        final Boolean         logged;
+        Optional<UserDetails> details;
 
         // Find the user
         try {
@@ -91,22 +123,7 @@ public final class DefaultLoginService implements LoginService {
             }
         }
 
-        // Generate token
-        token = generateToken(username, password);
-
-        status = new ImmutableLoginStatus(username, logged, token);
-
-        log.debug("Finished log in attempt for {}. Logged in: {}", username, logged);
-
-        return status;
-    }
-
-    private final String generateToken(final String username, final String password) {
-        final String rawToken;
-
-        rawToken = String.format("%s:%s", username, password);
-        return Base64.getEncoder()
-            .encodeToString(rawToken.getBytes(StandardCharsets.UTF_8));
+        return logged;
     }
 
 }
